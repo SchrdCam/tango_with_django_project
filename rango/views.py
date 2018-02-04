9affd1f6ddcabab89066581d7aad94ac23c93e56
Cameron Taylor <camt98@outlook.com>
2018-02-04 17:27:57 +0000

    Fixed some spelling mistakes

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from rango.models import Category, Page
+from rango.models import Category
+from rango.models import Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -12,15 +13,21 @@ def about(request):
     return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
+    
     context_dict ={}
+    
     try:
         category = Category.objects.get(slug=category_name_slug)
-        pages = Page.object.filter(category=category)
+        pages = Page.objects.filter(category=category)
+        print(Page.objects.filter(category=category))
         context_dict['pages'] = pages
         context_dict['category']= category
+        print(context_dict)
+        
     except Category.DoesNotExist:
         context_dict['category'] = None
         context_dict['pages'] = None
+        
     return render(request, 'rango/category.html', context_dict)
 
         
